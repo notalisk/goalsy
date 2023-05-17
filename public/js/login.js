81fa0fe15f6006ca660e65f50e7d0896e278f6af
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-17 16:24:38 -0500

    login workinggit add .git add .

--- public/js/login.js
+++ public/js/login.js
@@ -20,32 +20,6 @@ const loginFormHandler = async (event) => {
     }
 };
 
-const signupFormHandler = async (event) => {
-    event.preventDefault();
-
-    const email = document.querySelector('#signup-email').value.trim();
-    const username = document.querySelector('#signup-username').value.trim();
-    const password = document.querySelector('#signup-password').value.trim();
-
-    if (email && username && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ email, username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert(response.statusText);
-        }
-    }
-};
-
-// document
-//     .querySelector('.login-form')
-//     .addEventListener('submit', loginFormHandler);
-
 document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
